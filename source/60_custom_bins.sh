a986ea8fe4c2268428d764c53f85bb1bfd0d327a
Artur Roszczyk <artur.roszczyk@gmail.com>
2012-08-25 14:40:58 +0200

    Fix custom dirs

--- source/60_custom_bins.sh
+++ source/60_custom_bins.sh
@@ -6,7 +6,7 @@ CUSTOM_BINS_DIR=$HOME/.dotfiles/custom_bins
 # Call this function in the beginning of your prompt_command
 function __custom_bins_set_path() {
   local sed_filter relative_path
-  sed_filter="s#\\${HOME}/##g"
+  sed_filter="s#\\${HOME}##g"
   relative_path="$(pwd | sed ${sed_filter})"
 
   export PATH=$CUSTOM_BINS_DIR$relative_path:$DEFAULT_PATH
