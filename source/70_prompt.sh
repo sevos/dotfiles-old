f94954733fc58f1be517e16415eb28be4b40f682
Artur Roszczyk <artur.roszczyk@gmail.com>
2012-11-12 21:08:49 +0100

    Set pwd for tmux sessions

--- source/70_prompt.sh
+++ source/70_prompt.sh
@@ -6,6 +6,7 @@ trap 'prompt_stack=("${prompt_stack[@]}" "$BASH_COMMAND")' DEBUG
 
 function prompt_command() {
   export PS1="$(~/.dotfiles/libs/powerline-bash.py $?)"
+  PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#I_#P") "$PWD")'
 }
 
 PROMPT_COMMAND="prompt_command"
