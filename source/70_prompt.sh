9d2d45f056e4b1775f1d564ec5469ad72e5ecdd6
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-03-17 21:04:47 +0100

    Simple branch name

--- source/70_prompt.sh
+++ source/70_prompt.sh
@@ -4,7 +4,13 @@
 prompt_stack=()
 trap 'prompt_stack=("${prompt_stack[@]}" "$BASH_COMMAND")' DEBUG
 
+function parse_git_branch {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo " ("${ref#refs/heads/}")"
+}
+
 function prompt_command() {
+  PS1="\w\$(parse_git_branch)$"
   PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#I_#P") "$PWD")'
 }
 
