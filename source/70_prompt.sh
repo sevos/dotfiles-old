6ef725d64d61bc08b8cbf57183938468c9348cdb
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-03-17 20:42:11 +0100

    Remove powerline

--- source/70_prompt.sh
+++ source/70_prompt.sh
@@ -5,7 +5,6 @@ prompt_stack=()
 trap 'prompt_stack=("${prompt_stack[@]}" "$BASH_COMMAND")' DEBUG
 
 function prompt_command() {
-  export PS1="$(~/.dotfiles/libs/powerline-bash.py $?)"
   PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#I_#P") "$PWD")'
 }
 
