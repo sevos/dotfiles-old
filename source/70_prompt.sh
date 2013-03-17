90e680944b4b06ab3510e4f1af8b7c44864cabc2
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-03-17 20:48:22 +0100

    Install powerline again

--- source/70_prompt.sh
+++ source/70_prompt.sh
@@ -4,6 +4,8 @@
 prompt_stack=()
 trap 'prompt_stack=("${prompt_stack[@]}" "$BASH_COMMAND")' DEBUG
 
+. $HOME/.dotfiles/libs/powerline/powerline/bindings/bash/powerline.sh
+
 function prompt_command() {
   PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#I_#P") "$PWD")'
 }
