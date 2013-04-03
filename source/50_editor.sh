83046f742bdb78b3b7721a4492b46612528db75a
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-04-03 23:28:01 +0200

    Update

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,9 +1,7 @@
 # Editing
 
 if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
-  export EDITOR='subl -w'
-  export LESSEDIT='subl %f'
-  alias q='subl'
+  export EDITOR='vim'
 else
   export EDITOR=$(type vim vi nano pico 2>/dev/null | sed 's/ .*$//;q')
   alias q="$EDITOR -w -z"
